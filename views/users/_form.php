67d31cf0b9f5c03d51d63f81efacae965edfde47
Kush K Dhawan <Kush.Dhawan@in.ey.com>
2018-08-25 11:27:17 +0530

    Testing with Visual Studio Code

--- views/users/_form.php
+++ views/users/_form.php
@@ -22,23 +22,23 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'password')->passwordInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'password_salt')->textInput(['maxlength' => true]) ?>
+    <!-- <?= $form->field($model, 'password_salt')->textInput(['maxlength' => true]) ?>  -->
 
-    <?= $form->field($model, 'authKey')->textInput(['maxlength' => true]) ?>
+    <!-- <?= $form->field($model, 'authKey')->textInput(['maxlength' => true]) ?>    -->
 
     <?= $form->field($model, 'aadhaar_number')->textInput() ?>
 
     <?= $form->field($model, 'is_department_user')->dropDownList([ 'Y' => 'Y', 'N' => 'N', ], ['prompt' => '']) ?>
 
-    <?= $form->field($model, 'created_date_time')->textInput() ?>
+    <!-- <?= $form->field($model, 'created_date_time')->textInput() ?> -->
 
-    <?= $form->field($model, 'updated_date_time')->textInput() ?>
+    <!-- <?= $form->field($model, 'updated_date_time')->textInput() ?>  -->
 
-    <?= $form->field($model, 'remote_address')->textInput(['maxlength' => true]) ?>
+    <!-- <?= $form->field($model, 'remote_address')->textInput(['maxlength' => true]) ?> -->
 
-    <?= $form->field($model, 'user_agent')->textInput(['maxlength' => true]) ?>
+    <!-- <?= $form->field($model, 'user_agent')->textInput(['maxlength' => true]) ?> -->
 
-    <?= $form->field($model, 'is_active')->dropDownList([ 'Y' => 'Y', 'N' => 'N', ], ['prompt' => '']) ?>
+    <!-- <?= $form->field($model, 'is_active')->dropDownList([ 'Y' => 'Y', 'N' => 'N', ], ['prompt' => '']) ?> -->
 
     <div class="form-group">
         <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
